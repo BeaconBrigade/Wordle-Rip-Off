022ff75f29a130c88fa9499b6068ac6d4975bbba
Beacon Brigade <100320298+BeaconBrigade@users.noreply.github.com>
2022-03-25 13:17:21 +0000

    fixed issue with incorrect advertisement message

--- src/advertOptions.py
+++ src/advertOptions.py
@@ -1,6 +1,6 @@
 advertOptions = {
   'min' : ["Minecoins!", "Signup for Minecraft with code: \"wordleripoff\" for 20% off Minecoins!"],
   'hal' : ["Credits!", "Signup for Halo with code: \"wordleripoff\" for 20% off credits!"],
-  'sot' : ["Ancient Coins!", "Signup for Halo with code: \"wordleripoff\" for 20% off Ancient Coins!"],
+  'sot' : ["Ancient Coins!", "Signup for Sea of Thieves with code: \"wordleripoff\" for 20% off Ancient Coins!"],
   'apx' : ["Apex Coins!", "Signup for Apex with code \"wordleripoff\" for 20% off Apex Coins!"]
 }
